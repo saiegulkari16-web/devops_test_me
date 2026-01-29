5f898dc8dbf64cb5ac92dae09e18095ea5233096
saiegulkari16-web <saiegulkari16@gmail.com>
2026-01-29 17:25:39 +0530

    python file

---
+++ lab1.py
@@ -0,0 +1,47 @@
+#1
+# print('Enter the value of x: ', end = '')
+# x = int(input())
+# print('Therefore x is:',x)
+# print('x doubled is:', (2 * x))
+
+#2
+# print('NO PARKING')
+# print('2:00 - 6:00 a.m.')
+
+#3
+# user_name = input('Enter your name: ')
+# print('Hello', user_name , 'and welcome to the Python academy!')
+
+#4
+# first_name = input('Enter name: ' )
+# generic_location = input('Enter location: ')
+# whole_number = input('Enter a whole number: ')
+# plural_noun = input('Enter any plural: ')
+# print(first_name, 'went to', generic_location, 'for', whole_number, 'days, to buy', plural_noun ,'.')
+
+#5
+# print('Hello World')
+# print('How are you?')
+# print("(I'm fine)")   # when we use single quote in print use "" outside
+
+#6
+# first_number = int(input('Enter a number: '))
+# print('square of', first_number, 'is',(first_number * first_number), end=' ')
+# print('and cube is', (first_number * first_number * first_number))
+# second_number = int(input('Enter another number: '))
+# sum = first_number + second_number
+# product = first_number * second_number
+# print(first_number, "+", second_number, 'is', sum) 
+# #  always "" for + and * for printing variables values
+# print(first_number, "*", second_number, 'is', product)
+
+# # 7 draw a tree
+# print('   *   ')
+# print('  ***  ')
+# print(' ***** ')
+# print('*******')
+# print('  * *  ')  
+# print('  * *  ')
+# print('  * *  ') 
+
+#8(dought)
